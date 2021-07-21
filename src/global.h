7062aaa9e6af521ffb6712dbe7ab3ccae65f7b51
Dreamcat4 <dreamcat4@gmail.com>
2021-07-21 23:01:24 +0100

    move all #define global constants, from main.cpp --> global.h

--- src/global.h
+++ src/global.h
@@ -59,3 +59,74 @@ int calibrateTemp(bool type);
 
 
 
+//-----------------------------------------------------------------------------
+// global constants
+
+//LCD section addresses
+#define MAIN 17
+#define LEFT 25
+#define RIGHT 11
+#define SMALL 8
+
+//Touch buttons mapping
+#define UP 128
+#define DOWN 64
+#define SET 32
+#define CF 16
+
+//Pins
+// HT1621 ht(PB9, PA15, PA8, PB15); // LCD data,wr,rd,cs
+#define BACKLIGHT PB8
+#define TOUCHINT PA2 //touch interrupt
+#define PIEZO PA3
+#define REEDINT PB5 //reed switch pin
+#define BLOWER PB0 //blower pin
+#define HEATER PB3 //heater pin
+#define BTN1 PB12 //physical button 1
+#define BTN2 PB13 //physical button 2
+#define BTN3 PB14 //physical button 3
+#define AHEAT PA0 //temperature pot analog pin 
+#define ABLOW PA1 //blower pot analog pin                                          
+
+//thermocouple pins
+#define THERMO_CLK PA5
+#define THERMO_CS PA4
+#define THERMO_DO PA6
+// MAX6675 thermocouple(THERMO_CLK, THERMO_CS, THERMO_DO);
+
+//PID
+#define KP 3.2F
+#define KI 0.17F
+#define KD 1.5F
+#define HIGH_KP 3.2F
+#define HIGH_KI 0.08F
+#define HIGH_KD 0.32F
+
+//Miscellaneous
+#define FIRMWARE_VERSION 101 //firmware version (1.01)
+#define DEBOUNCETIME 10 //button debounce time
+#define LCDBRIGHTNESS 1 //default brightness, closer to 0 -> brighter, closer to 65535 -> dimmer
+#define LCDBRIGHTNESSDIM 25000 //standby brightness
+#define MINTEMP 100
+#define MAXTEMP 550
+#define MINBLOW 35
+#define MAXBLOW 100
+#define SHUTDOWNTEMP 85 //below this temp the blower shuts off
+#define SETTINGSEXITTIME 8000 //if you are inside settings (blinking screen), it will automatically exit after this time (ms)
+#define WINDOWSIZE 205 //Heater PWM period size (ms)
+#define SERIALTIME 500 //Serial output time (ms)
+
+//Calibration factors (so the air that comes OUT OF THE NOZZLE actually reaches the set temperature)
+#define RANGE200 1.05
+#define RANGE300 1.08
+#define RANGE400 1.11
+
+// touch IC address / registers
+#define touchAddress 0x56 // Device address
+#define keyValues 0x34 // Hexadecimal address for the key values register
+#define sysCon 0x3A //hex value for config register
+#define kdr0 0x23 //hex value for key disable register
+#define mcon 0x21 //hex value for mode control register
+#define gsr 0x20 //hex address for global sensitivity register
+
+
